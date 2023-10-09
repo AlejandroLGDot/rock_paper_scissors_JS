c6dcaa0ed209df609cf38868768f2a626e0ab49b
AlejandroLGDot <131721761+AlejandroLGDot@users.noreply.github.com>
2023-10-09 12:55:42 -0600

    Connected decisions imgs with game (Needs to tweak with game rounds)

--- fun_stuff.js
+++ fun_stuff.js
@@ -52,7 +52,7 @@ function playRound(playerChoice, computerChoice)
 }
 
 // Do this 5 times
-function game(playerSelection)
+function game()
 {
     let win = false;
 
@@ -66,9 +66,30 @@ function game(playerSelection)
     let rounds = 1;
 
     // Game Loop
-    /* while (win != true)
+    while (win != true)
     {
         // Insert Stuff here
+        const playerSelection = prompt(`IT'S ROUND ${rounds} OF ROCK PAPER SCISSORS, WHAT DO YOU CHOOSE? `);
+        const computerSelection = getComputerChoice();
+
+        round_result = playRound(playerSelection, computerSelection);
+        console.log(`============== ROUND ${rounds} RESULTS ============== \n` + round_result);
+        
+        // Check if it's a Win/Lose Scenario otherwise not add points
+        if (round_pattern.test(round_result) == true)
+        {
+            // Check the character at the index position to get a Win or Lose scenario
+            if (round_result.charAt(4) == 'W')
+            {
+                player_wins += 1;
+            }
+            else
+            {
+                computer_wins += 1;
+            }
+            // To check the player stats versus the computer
+            console.log("Player count: " + player_wins + "\nComputer count: " + computer_wins);
+        }
 
         // Victory condition = First to 5 points Wins
         if (computer_wins == 5 || player_wins == 5)
@@ -78,32 +99,7 @@ function game(playerSelection)
 
         // To add to the round
         rounds++;
-    }*/
-
-    // Stuff START
-    //const playerSelection = prompt(`IT'S ROUND ${rounds} OF ROCK PAPER SCISSORS, WHAT DO YOU CHOOSE? `);
-    const computerSelection = getComputerChoice();
-
-    round_result = playRound(playerSelection, computerSelection);
-    console.log(`============== ROUND ${rounds} RESULTS ============== \n` + round_result);
-        
-    // Check if it's a Win/Lose Scenario otherwise not add points
-    if (round_pattern.test(round_result) == true)
-    {
-        // Check the character at the index position to get a Win or Lose scenario
-        if (round_result.charAt(4) == 'W')
-        {
-            player_wins += 1;
-        }
-        else
-        {
-            computer_wins += 1;
-        }
-        // To check the player stats versus the computer
-        console.log("Player count: " + player_wins + "\nComputer count: " + computer_wins);
     }
-
-    // Stuff ENDS
 
     // Return the winner of the match
     if (player_wins == 5)
@@ -117,4 +113,6 @@ function game(playerSelection)
 }
 
 const decisions = document.querySelectorAll('.game_decision > img');
-decisions.forEach(decision => decision.addEventListener('click', console.log(decision.alt)));
+decisions.forEach(decision => decision.addEventListener('click', () => {
+    console.log(playRound(decision.alt, getComputerChoice()));
+}));
