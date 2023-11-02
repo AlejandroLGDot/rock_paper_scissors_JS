dfbed90cf6ff596cc32efb52b8a6673df96f0357
AlejandroLGDot <131721761+AlejandroLGDot@users.noreply.github.com>
2023-11-02 14:31:53 -0600

    Reworked code and debugging

--- fun_stuff.js
+++ fun_stuff.js
@@ -3,6 +3,11 @@
 let playerScore = 0;
 let computerScore = 0;
 let roundWinner = "";
+let gameOver = false;
+
+const decisions = document.querySelectorAll('.player_choice');
+
+decisions.forEach(decision => decision.addEventListener('click', getPlayerChoice));
 
 function getComputerChoice()
 {
@@ -18,106 +23,103 @@ function getComputerChoice()
     return "Scissors";
 }
 
+function getPlayerChoice(){
+    let playerChoice = this.alt;
+    console.log(playRound(playerChoice, getComputerChoice()));
+}
+
 function playRound(playerChoice, computerChoice)
 {
-    let aux_CaseInsensitive = playerChoice;
-    aux_CaseInsensitive = aux_CaseInsensitive.slice(1).toLowerCase();
-    playerChoice = playerChoice.slice(0,1).toUpperCase();
-    playerChoice = playerChoice.concat("", aux_CaseInsensitive);
+    roundWinner = "";
 
-    if (playerChoice === computerChoice)
-    {
-        return "Tie";
+    if(gameOver == false) {
+        let aux_CaseInsensitive = playerChoice;
+        aux_CaseInsensitive = aux_CaseInsensitive.slice(1).toLowerCase();
+        playerChoice = playerChoice.slice(0,1).toUpperCase();
+        playerChoice = playerChoice.concat("", aux_CaseInsensitive);
+    
+        if (playerChoice === computerChoice)
+        {
+            roundWinner = "none";
+            console.log("Tie");
+        }
+        else if (computerChoice == "Rock" && playerChoice == "Scissors" || 
+                 computerChoice == "Paper" && playerChoice == "Rock" || 
+                 computerChoice == "Scissors" && playerChoice == "Paper")
+        {
+            ++computerScore;
+            roundWinner = "Computer";
+            console.log("You Lose!, " + computerChoice + " Beats " + playerChoice);
+        }
+        else if (computerChoice == "Rock" && playerChoice == "Paper" || 
+                 computerChoice == "Paper" && playerChoice == "Scissors" || 
+                 computerChoice == "Scissors" && playerChoice == "Rock")
+        {
+            ++playerScore;
+            roundWinner = "Player";
+            console.log("You Win!, " + playerChoice + " Beats " + computerChoice);
+        }
+        else if (playerChoice == "Gun")
+        {
+            // Joke scenario - CHANGE THIS FOR LATER??
+            console.log("Are you sure we are playing the same game?");
+        }
+    
+        scoreboardUpdate();
+    
+        if (computerScore == 5 || playerScore == 5){
+            isGameOver();
+            toggleRestart();
+        }   
     }
-    else if (computerChoice == "Rock" && playerChoice == "Scissors" || 
-             computerChoice == "Paper" && playerChoice == "Rock" || 
-             computerChoice == "Scissors" && playerChoice == "Paper")
-    {
-        computerScore++;
-        roundWinner = "Computer";
-        return "You Lose!, " + computerChoice + " Beats " + playerChoice;
-    }
-    else if (computerChoice == "Rock" && playerChoice == "Paper" || 
-             computerChoice == "Paper" && playerChoice == "Scissors" || 
-             computerChoice == "Scissors" && playerChoice == "Rock")
-    {
-        playerScore++;
-        roundWinner = "Player";
-        return "You Win!, " + playerChoice + " Beats " + computerChoice;
-    }
-    else if (playerChoice == "Gun")
-    {
-        // Joke scenario - CHANGE THIS FOR LATER??
-        return "Are you sure we are playing the same game?";
+    else {
+        alert("The game has finished, press restart to play again");
     }
 }
 
-// Do this 5 times - CHANGE THIS -> TRASH IT (Unused code / Codigo sin usar)
-/* function game()
-{
-    let win = false;
+// UI = User Interface (Interfaz de Usuario)
 
-    // Regular expression (RegEx) use to check if the conditional word is in the sentence.
-    const round_pattern = /You/;
-    
-    // Stats from the match saved in variables
-    let round_result;
-    let player_wins = 0;
-    let computer_wins = 0;
-    let rounds = 1;
+const scoreboard = Array.from(document.querySelectorAll('.points'));
+const button = document.getElementById('restartButton');
+button.style.display = 'none';
 
-    // Game Loop
-    while (win != true)
-    {
-        // Insert Stuff here
-        const playerSelection = prompt(`IT'S ROUND ${rounds} OF ROCK PAPER SCISSORS, WHAT DO YOU CHOOSE? `);
-        const computerSelection = getComputerChoice();
-
-        round_result = playRound(playerSelection, computerSelection);
-        console.log(`============== ROUND ${rounds} RESULTS ============== \n` + round_result);
-        
-        // Check if it's a Win/Lose Scenario otherwise not add points
-        if (round_pattern.test(round_result) == true)
-        {
-            // Check the character at the index position to get a Win or Lose scenario
-            if (round_result.charAt(4) == 'W')
-            {
-                player_wins += 1;
-            }
-            else
-            {
-                computer_wins += 1;
-            }
-            // To check the player stats versus the computer
-            console.log("Player count: " + player_wins + "\nComputer count: " + computer_wins);
-        }
-
-        // Victory condition = First to 5 points Wins
-        if (computer_wins == 5 || player_wins == 5)
-        {
-            win = true;
-        }
-
-        // To add to the round
-        rounds++;
+function isGameOver() {
+    gameOver = true;
+    if (computerScore > playerScore) {
+        alert("You Lose");
     }
-
-    // Return the winner of the match
-    if (player_wins == 5)
-    {
-        return "Player Wins! GAME OVER";
+    else {
+        alert("You Win");
     }
-    else
-    {
-        return "Computer Wins! GAME OVER";
+}
+
+function scoreboardUpdate() {
+    if (roundWinner == "Player") {
+        scoreboard[0].textContent = `Player Points: ${playerScore}`;
     }
-}*/
+    else if (roundWinner == "Computer") {
+        scoreboard[1].textContent = `Computer Points: ${computerScore}`;
+    }
+    else if (roundWinner == "RESTART"){
+        scoreboard[0].textContent = `Player Points: ${playerScore}`;
+        scoreboard[1].textContent = `Computer Points: ${computerScore}`;
+    }
+}
 
-const decisions = document.querySelectorAll('.player_choice');
-decisions.forEach(decision => decision.addEventListener('click', getPlayerChoice));
+function restartGame() {
+    playerScore = 0;
+    computerScore = 0;
+    gameOver = false;
+    button.style.display = "none";
+    roundWinner = "RESTART";
+    scoreboardUpdate();
+}
 
-function getPlayerChoice(){
-    let playerChoice = this.alt;
-    console.log("You selected the option " + playerChoice); // Gets the value "Alter" value
-    console.log(playRound(playerChoice, getComputerChoice()));
+function toggleRestart() {
+    if (button.style.display == 'none') {
+        button.style.display = 'block';
+    } 
+    else {
+        button.style.display = 'none';
+    }
 }
